b74a9da321d1723c0f5afe20b93cd60dad299dba
Jeff Klemens <klemensj@philau.edu>
2016-07-12 15:02:58 -0400

    preliminary ANOVAS to check significance pattern for all responses

--- Data Exploration pairplots.R
+++ Data Exploration pairplots.R
@@ -14,3 +14,28 @@ Student_Numerical_Fall <- subset(Numerical_Fall, Group == "Student")
 pairs.panels(Faculty_Numerical_Fall[,2:7], main = "Faculty All Responses")
 pairs.panels(Student_Numerical_Fall[,2:7], main = "Students All Responses")
 
+ #  ANOVA of everything together
+
+engage <- as.numeric(Numerical_Fall$Engage)
+video_ANOVA<- lm(engage ~ Numerical_Fall$Video*Numerical_Fall$Group)
+anova(video_ANOVA_engage)
+
+extract <- as.numeric(Numerical_Fall$Extract)
+video_ANOVA <- lm(extract ~ Numerical_Fall$Video*Numerical_Fall$Group)
+anova(video_ANOVA)
+
+enjoy <- as.numeric(Numerical_Fall$Enjoy)
+video_ANOVA <- lm(enjoy ~ Numerical_Fall$Video*Numerical_Fall$Group)
+anova(video_ANOVA)
+
+retain <- as.numeric(Numerical_Fall$Retain)
+video_ANOVA <- lm(retain ~ Numerical_Fall$Video*Numerical_Fall$Group)
+anova(video_ANOVA)
+
+context <- as.numeric(Numerical_Fall$Context)
+video_ANOVA <- lm(context ~ Numerical_Fall$Video*Numerical_Fall$Group)
+anova(video_ANOVA)
+
+maintain <- as.numeric(Numerical_Fall$Maintain)
+video_ANOVA <- lm(maintain ~ Numerical_Fall$Video*Numerical_Fall$Group)
+anova(video_ANOVA)
