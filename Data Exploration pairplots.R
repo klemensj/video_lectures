e6f0829ac9ec09a2aba35270446561db2968bef6
Jeff Klemens <klemensj@philau.edu>
2016-07-14 23:18:15 -0500

    ANOVAS and means for comparison of fall spring student data

--- Data Exploration pairplots.R
+++ Data Exploration pairplots.R
@@ -10,7 +10,7 @@ library("psych", lib.loc="~/Library/R/3.2/library")
 
 ## Read in dataset that is all of the numerical data from the initial (fall) survey
 
-Numerical_Fall <- read.csv("data/Numerical_Fall.csv")
+Numerical_Fall <- read.csv("data/Numerical_Fall.csv", header = TRUE, na.strings=".", check.names=FALSE)
 
 ## Separate into faculty and student
 
@@ -48,11 +48,45 @@ maintain <- as.numeric(Numerical_Fall$Maintain)
 video_ANOVA <- lm(maintain ~ Numerical_Fall$Video*Numerical_Fall$Group)
 anova(video_ANOVA)
 
-## From here on idea is to do ANOVA and create comparisons of 
-## Longform with regular student data just take AllStudentData , 
-## create a bunch of new columns based on numbers
+## Created a new dataset in excel of Longform data in numerical form Numerical_Spring.csv
 
-Numerical_Spring<-subset
+Numerical_Spring<-read.csv("data/Numerical_Spring.csv", header = TRUE, na.strings=".", check.names=FALSE)
+
+## Bind together with fall student data
+
+SpringFallStudent<-rbind(Student_Numerical_Fall,Numerical_Spring)
+
+##ANOVAS comparing fall and spring videos
+
+engage <- as.numeric(SpringFallStudent$Engage)
+video_ANOVA<- lm(engage ~ SpringFallStudent$Video*SpringFallStudent$Group)
+anova(video_ANOVA)
+
+extract <- as.numeric(SpringFallStudent$Extract)
+video_ANOVA <- lm(extract ~ SpringFallStudent$Video*SpringFallStudent$Group)
+anova(video_ANOVA)
+
+enjoy <- as.numeric(SpringFallStudent$Enjoy)
+video_ANOVA <- lm(enjoy ~ SpringFallStudent$Video*SpringFallStudent$Group)
+anova(video_ANOVA)
+
+retain <- as.numeric(SpringFallStudent$Retain)
+video_ANOVA <- lm(retain ~ SpringFallStudent$Video*SpringFallStudent$Group)
+anova(video_ANOVA)
+
+context <- as.numeric(SpringFallStudent$Context)
+video_ANOVA <- lm(context ~ SpringFallStudent$Video*SpringFallStudent$Group)
+anova(video_ANOVA)
+
+maintain <- as.numeric(SpringFallStudent$Maintain)
+video_ANOVA <- lm(maintain ~ SpringFallStudent$Video*SpringFallStudent$Group)
+anova(video_ANOVA)
+
+## Means for use in generating interaction plot
+
+springave<-apply(Numerical_Spring[,2:7],2,mean, na.rm=TRUE)
+fallave<-apply(Student_Numerical_Fall[,2:7],2,mean, na.rm=TRUE)
+
 
 ## t test of the test scores
 
